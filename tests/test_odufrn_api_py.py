ecc6ba24053d4ae9ec7f09696f29398994c58f3d
João Pedro Limão <jplimao12@gmail.com>
2019-10-05 13:54:25 -0300

    Retirando API_VERSION do arquivo .travis.yml

--- tests/test_odufrn_api_py.py
+++ tests/test_odufrn_api_py.py
@@ -13,8 +13,8 @@ class ODUFRNApi(unittest.TestCase):
         self.api_ufrn = UfrnApi(
             env('CLIENT_ID'),
             env('CLIENT_SECRET'),
-            env('X_API_KEY'),
-            env('API_VERSION'))
+            env('X_API_KEY'), 
+            'v1')
 
     def test_can_print_resources(self):
         """Verifica se consegue mostrar na tela os recursos presentes na API
